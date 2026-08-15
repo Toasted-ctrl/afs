c1c3ee18306735d67e0fc93fc46d640bd1c45fe9
Mick <themickbb@gmail.com>
2026-08-15 10:58:54 +0200

    Initiial create.

---
+++ dags/test_dag.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.sdk import task
+from datetime import datetime
+from logging import getLogger
+
+
+# -------------------------------------------------------------------------
+# This is a test dag you can run to check if AirFlow is working as expected
+# -------------------------------------------------------------------------
+
+log = getLogger(__name__)
+
+
+@task.python
+def write_test_log() -> str:
+
+    log.info("AirFlow is working properly.")
+    log.info("Returning test value")
+
+    return 'test_value'
+
+
+with DAG(
+    dag_id="Test.AirFlow.Operational",
+    dag_display_name="Test: AirFlow Operational",
+    start_date=datetime(2026, 8, 14),
+    schedule="@daily",
+    catchup=False
+) as dag_daily:
+
+    tv = write_test_log()
+    log.info(f"Received test value: {tv}")
+    log.info("Shutting down test.")
